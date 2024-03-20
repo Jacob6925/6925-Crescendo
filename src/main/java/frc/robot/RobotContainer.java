1eccab092d14fd53490030eaead886c69c606d39
jacob <mcman1013.2@gmail.com>
2024-03-20 17:26:52 -0400

    last minute changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,10 +108,12 @@ public class RobotContainer {
 
         new JoystickButton(operator, 1).whileTrue(new IntakeCommand(s_Intake, null, IntakeConstants.IndexerSpeed.FEED_SHOOTER)); // 1 - feed shooter
         new JoystickButton(operator, 2).whileTrue(new ShooterCommand(s_Shooter, -0.85, -0.85)); // 2 - start shooter
-        new JoystickButton(operator, 3).whileTrue(new ClimberCommand(s_Climber, -0.3, 0)); // 3 - left climber down
-        new JoystickButton(operator, 4).whileTrue(new ClimberCommand(s_Climber, 0, -0.3)); // 4 - right climber down
-        new JoystickButton(operator, 5).whileTrue(new ClimberCommand(s_Climber, 0.3, 0)); // 5 - left climber up
-        new JoystickButton(operator, 6).whileTrue(new ClimberCommand(s_Climber, 0, 0.3)); // 6 - right climber up
+        // new JoystickButton(operator, 3).whileTrue(new InstantCommand(() -> s_Climber.setLeft(-0.3))); // 3 - left climber down
+        // new JoystickButton(operator, 4).whileTrue(new InstantCommand(() -> s_Climber.setRight(-0.3))); // 4 - right climber down
+        // new JoystickButton(operator, 5).whileTrue(new InstantCommand(() -> s_Climber.setLeft(0.3))); // 5 - left climber up
+        // new JoystickButton(operator, 6).whileTrue(new InstantCommand(() -> s_Climber.setRight(0.3))); // 6 - right climber up
+        new JoystickButton(operator, 3).whileTrue(new ShooterCommand(s_Shooter, -0.3, -0.3)); // shooter down
+        new JoystickButton(operator, 5).whileTrue(new ShooterCommand(s_Shooter, 0.3, 0.3)); // shooter up
         new JoystickButton(operator, 7).onTrue(new IntakeCommand(s_Intake, PivotState.AMP, IndexerSpeed.NONE)); // 7 - amp pivot
         new JoystickButton(operator, 8).whileTrue(new IntakeCommand(s_Intake, PivotState.NONE, IndexerSpeed.AMP)); // 8 - amp indexer
         new JoystickButton(operator, 9).whileTrue(new IntakeCommand(s_Intake, null, IndexerSpeed.INTAKE)); // 9 - intake (centering)

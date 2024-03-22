4e2b27d7ad2d5dc844b59e20bd1c7702ccb28bf9
jacob <mcman1013.2@gmail.com>
2024-03-22 13:40:38 -0400

    fix import

--- src/main/java/frc/robot/subsystems/SwerveSubsys.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsys.java
@@ -23,6 +23,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsys extends SubsystemBase {
